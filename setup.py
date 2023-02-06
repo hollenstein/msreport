4cd83b90249ed77189302db7c27ffb79de984d81
David M. Hollenstein <hollenstein.david@gmail.com>
2023-02-06 16:13:51 +0100

    build: Switch from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-VERSION = "0.0.6"
-
-packages = find_packages()
-packages.append("msreport.rinterface.rscripts")
-packages.append("msreport_scripts.benchmark")
-packages.append("msreport_scripts.excel_report")
-
-setup(
-    name="msreport",
-    version=VERSION,
-    license="Apache v2",
-    author="David M. Hollenstein",
-    author_email="hollenstein.david@gmail.com",
-    install_requires=[
-        "adjustText",
-        "matplotlib",
-        "numpy",
-        "pandas",
-        "pyteomics",
-        "rpy2>=3.5.3",
-        "scipy",
-        "seaborn",
-        "sklearn",
-        "statsmodels",
-        "xlsxreport>=0.0.5",
-    ],
-    extras_require={
-        "test": ["pytest"],
-    },
-    python_requires=">=3.9",
-    packages=packages,
-    package_data={"msreport.rinterface.rscripts": ["*.r", "*.R"]},
-    keywords=["proteomics", "mass spectrometry", "data analysis", "data processing"],
-)
+setup()
