fix: Fix iBAQ calculation issue with negative iBAQ peptide numbers

msreport/reader.py
@@ -1407,6 +1407,8 @@ def add_ibaq_intensities(
         normalize: Scales iBAQ intensities per sample so that the sum of all iBAQ
             intensities is equal to the sum of all Intensities.
         ibaq_peptide_column: Column in 'table' containing the number of iBAQ peptides.
+            No iBAQ intensity is calculated for rows with negative values or zero in the
+            ibaq_peptide_column.
         intensity_tag: Substring used to identify intensity columns from the 'table'
             that are used to calculate iBAQ intensities.
         ibaq_tag: Substring used for naming the new 'table' columns containing the
@@ -1415,11 +1417,18 @@ def add_ibaq_intensities(
     """
     for intensity_column in helper.find_columns(table, intensity_tag):
         ibaq_column = intensity_column.replace(intensity_tag, ibaq_tag)
-        table[ibaq_column] = table[intensity_column] / table[ibaq_peptide_column]
+        valid = table[ibaq_peptide_column] > 0
+
+        table[ibaq_column] = np.nan
+        table.loc[valid, ibaq_column] = (
+            table.loc[valid, intensity_column] / table.loc[valid, ibaq_peptide_column]
+        )
 
         if normalize:
-            factor = table[intensity_column].sum() / table[ibaq_column].sum()
-            table[ibaq_column] = table[ibaq_column] * factor
+            total_intensity = table.loc[valid, intensity_column].sum()
+            total_ibaq = table.loc[valid, ibaq_column].sum()
+            factor = total_intensity / total_ibaq
+            table.loc[valid, ibaq_column] = table.loc[valid, ibaq_column] * factor
 
 
 def add_peptide_positions(

tests/test_reader.py
@@ -304,7 +304,9 @@ class TestAddIbaqIntensities:
             ([150, 150], True),
         ],
     )
-    def test_correct_ibaq_intensities(self, expected_ibaq, normalize_intensity):
+    def test_correct_calculation_of_ibaq_intensities(
+        self, expected_ibaq, normalize_intensity
+    ):
         msreport.reader.add_ibaq_intensities(
             self.table,
             normalize=normalize_intensity,
@@ -312,7 +314,32 @@ class TestAddIbaqIntensities:
             intensity_tag="intensity",
             ibaq_tag="ibaq",
         )
-        assert np.all(np.equal(self.table["ibaq"], expected_ibaq))
+        np.testing.assert_array_equal(self.table["ibaq"], np.array(expected_ibaq))
+
+    @pytest.mark.parametrize(
+        "expected_ibaq, normalize_intensity",
+        [
+            ([50, 50, np.nan, np.nan], False),
+            ([150, 150, np.nan, np.nan], True),
+        ],
+    )
+    def test_calculation_with_invalid_ibaq_peptides(
+        self, expected_ibaq, normalize_intensity
+    ):
+        table = pd.DataFrame(
+            {
+                "ibaq_petides": [2, 4, 0, -1],
+                "intensity": [100.0, 200.0, 50.0, 50.0],
+            }
+        )
+        msreport.reader.add_ibaq_intensities(
+            table,
+            normalize=normalize_intensity,
+            ibaq_peptide_column="ibaq_petides",
+            intensity_tag="intensity",
+            ibaq_tag="ibaq",
+        )
+        np.testing.assert_array_equal(table["ibaq"], np.array(expected_ibaq))
 
 
 class TestResultReader: