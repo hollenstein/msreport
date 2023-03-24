06939d080eb0d69108de17c3b7843d01811bbac3
David M. Hollenstein <hollenstein.david@gmail.com>
2023-03-24 12:14:00 +0100

    feat: Make relevant msreport submodules availale by importing `msreport`

--- msreport/__init__.py
+++ msreport/__init__.py
@@ -1,5 +1,12 @@
 from msreport.qtable import Qtable
-from msreport.reader import MaxQuantReader, FragPipeReader
+from msreport.reader import MaxQuantReader, FragPipeReader, SpectronautReader
 from msreport.helper import importProteinDatabase, import_protein_database
+
+import msreport.analyze
+import msreport.export
+import msreport.impute
+import msreport.normalize
+import msreport.plot
+import msreport.reader
 
 __version__ = "0.0.10"
