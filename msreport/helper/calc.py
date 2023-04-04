fix: Mode calculation not working with nan or all identical values.

msreport/helper/calc.py
@@ -43,19 +43,27 @@ def mode(values: Iterable) -> float:
     """Calculate the mode by using kernel-density estimation.
 
     Args:
-        values: Sequence of values for which the mode will be estimated.
+        values: Sequence of values for which the mode will be estimated, only finite
+            values are used for the calculation.
 
     Returns:
-        The estimated mode.
+        The estimated mode. If no finite values are present, returns nan.
     """
-    median = np.median(values)
-    bounds = (median - 1.5, median + 1.5)
-    kde = scipy.stats.gaussian_kde(values)
-    optimize_result = scipy.optimize.minimize_scalar(
-        lambda x: -kde(x)[0], method="Bounded", bounds=bounds
-    )
-    mode = optimize_result.x
-    # Maybe add fallback function if optimize was not successful
+    values = np.array(values)
+    finite_values = values[np.isfinite(values)]
+    if len(finite_values) == 0:
+        mode = np.nan
+    elif len(np.unique(finite_values)) == 1:
+        mode = np.unique(finite_values)[0]
+    else:
+        median = np.median(finite_values)
+        bounds = (median - 1.5, median + 1.5)
+        kde = scipy.stats.gaussian_kde(finite_values)
+        optimize_result = scipy.optimize.minimize_scalar(
+            lambda x: -kde(x)[0], method="Bounded", bounds=bounds
+        )
+        mode = optimize_result.x
+        # Maybe add fallback function if optimize was not successful
     return mode
 
 

tests/test_helper.py
@@ -254,11 +254,30 @@ def test_intensities_in_logspace(data, data_in_logspace):
     assert msreport.helper.intensities_in_logspace(data) == data_in_logspace
 
 
-def test_mode():
-    values = np.random.normal(size=100)
-    mode = msreport.helper.mode(values)
-    assert isinstance(mode, float)
-    assert mode < 1 and mode > -1
+class TestMode:
+    @pytest.fixture(autouse=True)
+    def _init_random_values(self):
+        np.random.seed(0)
+        self.values = np.random.normal(size=100)
+
+    def test_mode_is_calculated_properly(self):
+        mode = msreport.helper.mode(self.values)
+        np.testing.assert_allclose(mode, 0.0879, rtol=1e-02, atol=1e-02, equal_nan=True)  # fmt: skip
+
+    def test_mode_calculation_with_some_nan_returns_a_number(self):
+        self.values[[i for i in range(1, 100, 10)]] = np.nan
+        mode = msreport.helper.mode(self.values)
+        assert ~np.isnan(mode)
+
+    def test_mode_calculation_with_all_nan_returns_nan(self):
+        self.values[:] = np.nan
+        mode = msreport.helper.mode(self.values)
+        assert np.isnan(mode)
+
+    def test_mode_calculation_with_all_identical_values(self):
+        self.values[:] = 1
+        mode = msreport.helper.mode(self.values)
+        assert mode == 1
 
 
 def test_calculate_tryptic_ibaq_peptides():