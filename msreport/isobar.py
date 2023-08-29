57f27f1f9564bc325f539fed8fdbd4a3c569a0b7
David M. Hollenstein <hollenstein.david@gmail.com>
2023-08-29 16:13:34 +0200

    feat: Add functions to apply and remove isotope impurity contamination

---
+++ msreport/isobar.py
@@ -0,0 +1,15 @@
+import numpy as np
+import scipy
+
+
+def _apply_isotope_impurity_contamination(
+    intensities: np.array, impurity_matrix: np.array
+) -> np.array:
+    return np.sum(impurity_matrix * intensities, axis=1)
+
+
+def _correct_isotope_impurity_contamination(
+    intensities: np.array, impurity_matrix: np.array
+) -> np.array:
+    corrected_intensities, _ = scipy.optimize.nnls(impurity_matrix, intensities)
+    return corrected_intensities
