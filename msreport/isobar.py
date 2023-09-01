e29c3f50c9743f4910aa4ca3eae04e16f00e6dd1
David M. Hollenstein <hollenstein.david@gmail.com>
2023-09-01 10:47:28 +0200

    feat: Add function to perform impurity correction on a table

--- msreport/isobar.py
+++ msreport/isobar.py
@@ -1,5 +1,42 @@
+import functools
 import numpy as np
 import scipy
+
+import msreport.helper
+
+
+def correct_isobaric_reporter_impurities(
+    intensity_table: np.array,
+    diagonal_impurity_matrix: np.array,
+) -> np.array:
+    """Performs isotope impurity correction on isobaric reporter expression values.
+
+    Args:
+        intensity_table: A two-dimenstional array with columns corresponding to isobaric
+            reporter channels and rows to measured units such as PSMs, peptides or
+            proteins.
+        diagonal_impurity_matrix: A reporter isotope impurity matrix in a diagonal
+            format, where columns describe the isotope impurity of a specific channel,
+            and the values in each row indicate the percentage of signal from the
+            reporter that is present in each channel.
+    """
+    # TODO: not tested #
+    apply_impurity_correction = functools.partial(
+        correct_isotope_impurity_contamination,
+        impurity_matrix=diagonal_impurity_matrix,
+    )
+
+    data_was_in_logpsace = msreport.helper.intensities_in_logspace(intensity_table)
+
+    if data_was_in_logpsace:
+        intensity_table = np.power(2, intensity_table)
+    intensity_table[np.isnan(intensity_table)] = 0
+    corrected_table = np.apply_along_axis(apply_impurity_correction, 1, intensity_table)
+    corrected_table[corrected_table <= 0] = 0
+    if data_was_in_logpsace:
+        corrected_table = np.log2(corrected_table)
+
+    return corrected_table
 
 
 def apply_isotope_impurity_contamination(
