143f79dc4a97bd189d26919396977b843a4cd0ad
David M. Hollenstein <hollenstein.david@gmail.com>
2026-02-05 13:27:45 +0100

    refactor: Define public API in __init__.py using '__all__'

--- msreport/__init__.py
+++ msreport/__init__.py
@@ -1,11 +1,20 @@
-import msreport.analyze
-import msreport.export
-import msreport.impute
-import msreport.normalize
-import msreport.plot
-import msreport.reader
+from msreport import analyze, export, impute, normalize, plot, reader
 from msreport.fasta import import_protein_database
 from msreport.qtable import Qtable
 from msreport.reader import FragPipeReader, MaxQuantReader, SpectronautReader
 
 __version__ = "0.0.32"
+
+__all__ = [
+    "analyze",
+    "export",
+    "impute",
+    "normalize",
+    "plot",
+    "reader",
+    "import_protein_database",
+    "Qtable",
+    "FragPipeReader",
+    "MaxQuantReader",
+    "SpectronautReader",
+]
